55f1016c3af3fb7ef765596ef954af91b649fd76
icgood <icgood@users.noreply.github.com>
2023-05-14 14:49:03 +0000

    deploy: 6d79d86be6e0077e13bf2d6feb49ecd0120606bf

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.0rc0',
+    VERSION: '0.11.0rc1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
